21c07380fcc6531ba31dbf942ba12d87505dd1d7
imina-malak <91422019+imina-malak@users.noreply.github.com>
2021-10-27 14:06:41 +0200

    init

--- website/app.js
+++ website/app.js
@@ -4,52 +4,46 @@ let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 /* Global Variables */
 let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip='
 const apiKey ="2526672c019a649b32d1fca91317d30e"
-
+// when we click on genetate button 
 document.getElementById('generate').addEventListener("click", Go);
-
 function Go(e){
+    // get the data from inputs
     const zip =  document.getElementById('zip').value;
     const feelings =  document.getElementById('feelings').value;
-
     getWeather(baseURL, zip , apiKey)
     .then(function(data){
         postData('/add',{date:newDate,temp:data.main.temp,content:feelings}
         ).then(updateUI())
     })
-    
 }
-
-async function getWeather(baseURL, zip , apiKey) {
+// get the data from API
+async function getWeather(baseURL, zip , apiKey){
     const response = await fetch(`${baseURL}${zip},us&appid=${apiKey}`)
     try{
         const data = await response.json();
         return data;
-    }catch(error) {
+    }catch(error){
         console.log("error", error);
     }
 }
-
-
-// send data to the server 
-async function postData (url = '',data = {}) {
+// send data to the server
+async function postData (url = '',data = {}){
     const response = await fetch(url, {
-    method: 'POST', 
+    method: 'POST',
     credentials: 'same-origin',
     headers: {
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data), 
+    body: JSON.stringify(data),
     });
-    try {
+    try{
         const newData = await response.json();
         console.log(newData);
         return newData;
-    }catch(error) {
+    }catch(error){
     console.log("error", error);
     }
 }
-
-
 //to Update the data
 async function updateUI() {
     const req = await fetch('/all')
